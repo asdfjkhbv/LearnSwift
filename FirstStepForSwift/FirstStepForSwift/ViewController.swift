3e4f723cb795f83261957755202874a8eb5bd453
ma yan <376126778@qq.com>
2016-04-17 14:27:20 +0800

    add files for server
    init to add files

---
+++ FirstStepForSwift/FirstStepForSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstStepForSwift
+//
+//  Created by 马岩 on 16/4/17.
+//  Copyright © 2016年 may. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
